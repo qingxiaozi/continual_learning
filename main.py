ef40fd5e27e6e51bd107352c256a8038f1aa252b
qingxiaozi <18298729936@qq.com>
2026-03-14 21:36:20 +0800

    add logger in main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import logging as logger
+logger.basicConfig(
+    level=logger.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 import json
 import time
 import os
