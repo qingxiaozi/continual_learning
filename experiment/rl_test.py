fd1658500e7d01991e4f9eb275546ccf4dda3197
qingxiaozi <18298729936@qq.com>
2026-03-19 10:35:02 +0800

    change log

--- experiment/rl_test.py
+++ experiment/rl_test.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
