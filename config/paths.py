cfd00fe2ec45a5ac846b5f9d082e5a6a880a9a9d
ruiqinghe <ruiqinghe@buaa.edu.cn>
2026-04-12 20:40:09 +0800

    new testing

--- config/paths.py
+++ config/paths.py
@@ -40,7 +40,7 @@ class Paths:
     def get_drl_model_path():
         dataset_name = Config.CURRENT_DATASET
         model_dir = os.path.join(Paths.RESULTS_PTH_DIR, dataset_name)
-        model_filename = f"trained_drl_model_{dataset_name}_4.pth"
+        model_filename = f"trained_drl_model_{dataset_name}_5.pth"
         full_path = os.path.join(model_dir, model_filename)
         os.makedirs(model_dir, exist_ok=True)
         return full_path
